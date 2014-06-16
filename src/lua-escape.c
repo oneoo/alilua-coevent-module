eab47bc8b4fc438b8dc986a943119c0eba9c883d
oneoo <oneoo@yo2.com>
2014-06-16 13:37:19 +0800

    update merry module & fix protocol in function urlencode

--- src/lua-escape.c
+++ src/lua-escape.c
@@ -8,7 +8,7 @@ int lua_f_escape_uri(lua_State *L)
     u_char  *src = NULL,
              *dst = NULL;
 
-    if(lua_gettop(L) != 1) {
+    if(lua_gettop(L) < 1) {
         return luaL_error(L, "expecting one argument");
     }
 
@@ -18,12 +18,16 @@ int lua_f_escape_uri(lua_State *L)
         return 1;
     }
 
-    escape = 2 * urlencode(NULL, src, len, ESCAPE_URI);
+    int raw_encode = 0;
+    if(lua_gettop(L) == 2 && lua_isboolean(L, 2)){
+        raw_encode = lua_toboolean(L, 2);
+    }
+
+    escape = 3 * len;
 
     if(escape) {
-        dlen = escape + len;
-        dst = lua_newuserdata(L, dlen);
-        urlencode(dst, src, len, ESCAPE_URI);
+        dst = lua_newuserdata(L, escape);
+        dlen = urlencode(dst, src, len, raw_encode==0?ESCAPE_URL:RAW_ESCAPE_URL);
         lua_pushlstring(L, (char *) dst, dlen);
     }
 
@@ -35,19 +39,21 @@ int lua_f_unescape_uri(lua_State *L)
     size_t  len = 0,
             dlen = 0;
     u_char  *p = NULL,
-             *src = NULL,
-              *dst = NULL;
+             *src = NULL;
 
-    if(lua_gettop(L) != 1) {
+    if(lua_gettop(L) < 1) {
         return luaL_error(L, "expecting one argument");
+    }
+
+    int raw_encode = 0;
+    if(lua_gettop(L) == 2 && lua_isboolean(L, 2)){
+        raw_encode = lua_toboolean(L, 2);
     }
 
     src = (u_char *) luaL_checklstring(L, 1, &len);
     /* the unescaped string can only be smaller */
-    dlen = len;
-    p = lua_newuserdata(L, dlen);
-    dst = p;
-    urldecode(&dst, &src, len, UNESCAPE_URI_COMPONENT);
-    lua_pushlstring(L, (char *) p, dst - p);
+    p = lua_newuserdata(L, len);
+    dlen = urldecode(&p, &src, len, raw_encode==0?UNESCAPE_URL:RAW_UNESCAPE_URL);
+    lua_pushlstring(L, (char *) p, dlen);
     return 1;
 }
